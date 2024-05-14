70562b7780457d42a721f0b36157d183084b0be9
colpa di cesco <146211072+jfbergamo@users.noreply.github.com>
2024-05-14 10:31:40 +0200

    Implementato fino al knn (da finire)

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Coding OCR con machine learning in Python
 
-[Database](https://it.wikipedia.org/wiki/MNIST_database)
+Semplice algoritmo di OCR per riconoscere numeri basato sul [database MNIST](https://it.wikipedia.org/wiki/MNIST_database) e implementato col [K-nearest neighbors](https://it.wikipedia.org/wiki/K-nearest_neighbors).
+
+Utilizzo:
+```console
+$ python3 ./main.py
+```
--- main.py
+++ main.py
@@ -35,7 +35,40 @@ def read_labels(filename: str, cap: int = None) -> list[int]:
 def get_data(images_path: str, labels_path: str, cap: int = None) -> tuple[list[list[list[int]]], list[int]]:
     return (read_images(images_path, cap), read_labels(labels_path, cap))
 
+def flatten(matrix: list[list[int]]) -> list[int]:
+    # STANNO INIZIANDO A FARMI IMPAZZIRE
+    return [pixel for row in matrix for pixel in row]
+
+def extract_features(images: list[list[list[int]]]) -> list[list[int]]:
+    return [flatten(img) for img in images]
+
+def distance(a: list[int], b: list[int]) -> int:
+    return sum(
+        [
+            (x - y) ** 2 for x, y in zip(a, b)
+        ]
+    ) ** 0.5
+
+def training_distances(test_img: list[int], train_images: list[int]) -> list[int]:
+    return [distance(test_img, train_img) for train_img in train_images]
+
+def knn(train_images: list[list[int]], train_labels: list[int], test_images: list[list[int]], k: int = 3):
+    predicted = []
+    for test_img in test_images:
+        train_dists = training_distances(test_img, train_images)
+        sorted_dists_idx = [
+            pair[0] for pair in sorted(enumerate(train_dists), key=lambda x: x[1])    
+        ]
+        candidates = [train_labels[i] for i in sorted_dists_idx[:k]]
+        predicted.append(5) # temporaneo
+    return predicted
+
 if __name__ == "__main__":
-    print("Avvio lettura dati")
-    (train_images, train_labels) = get_data(DATA_FILENAMES["TRAIN_IMAGES"], DATA_FILENAMES["TRAIN_LABELS"], 1000)
-    (test_images, test_labels) = get_data(DATA_FILENAMES["TEST_IMAGES"], DATA_FILENAMES["TEST_LABELS"], 1000)
+    print("Avvio lettura dati di training...")
+    (train_images, train_labels) = get_data(DATA_FILENAMES["TRAIN_IMAGES"], DATA_FILENAMES["TRAIN_LABELS"], 3)
+    print("Dati di training letti correttamente!")
+    print("Avvio lettura dati di test...")
+    (test_images, test_labels) = get_data(DATA_FILENAMES["TEST_IMAGES"], DATA_FILENAMES["TEST_LABELS"], 3)
+    print("Dati di test letti correttamente!")
+    print("Avvio calcolo...")
+    print(knn(extract_features(train_images), train_labels, extract_features(test_images), 7))
